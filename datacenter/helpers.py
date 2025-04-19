4468fb6f1c469dd0bf31007b4a6ac26c337fdb7c
Nikint86 <lobackiy_2007@mail.ru>
2025-04-19 14:17:26 +0300

    Избавился от "Магических чисел"

--- datacenter/helpers.py
+++ datacenter/helpers.py
@@ -1,10 +1,15 @@
 from django.utils.timezone import localtime
 
 
+SECONDS_PER_MINUTE = 60
+MINUTES_PER_HOUR = 60
+SECONDS_PER_HOUR = SECONDS_PER_MINUTE * MINUTES_PER_HOUR
+
+
 def format_duration(duration):
     total_seconds = int(duration.total_seconds())
-    hours = total_seconds // 3600
-    minutes = (total_seconds % 3600) // 60
+    hours = total_seconds // SECONDS_PER_HOUR
+    minutes = (total_seconds % SECONDS_PER_HOUR) // SECONDS_PER_MINUTE
     return f"{hours}:{minutes:02}"
 
 
@@ -12,4 +17,4 @@ def is_visit_long(visit, minutes=60):
     entered_at = localtime(visit.entered_at)
     leaved_at = localtime(visit.leaved_at) if visit.leaved_at else localtime()
     duration = leaved_at - entered_at
-    return duration.total_seconds() > minutes * 60
+    return duration.total_seconds() > minutes * SECONDS_PER_MINUTE
