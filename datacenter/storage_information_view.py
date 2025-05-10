516266ce03666c8c5eea6208824197eb014f1bff
Nikint86 <lobackiy_2007@mail.ru>
2025-05-10 16:39:08 +0300

    add code

--- datacenter/storage_information_view.py
+++ datacenter/storage_information_view.py
@@ -2,14 +2,7 @@ from datacenter.models import Passcard
 from datacenter.models import Visit
 from django.shortcuts import render
 from django.utils.timezone import localtime
-
-
-def format_duration(duration):
-    total_seconds = int(duration.total_seconds())
-    hours = total_seconds // 3600
-    minutes = (total_seconds % 3600) // 60
-    seconds = total_seconds % 60
-    return f"{hours}:{minutes:02}:{seconds:02}"
+from helpers import format_duration
 
 
 def storage_information_view(request):
